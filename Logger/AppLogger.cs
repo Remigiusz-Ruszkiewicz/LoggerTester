57edbb9832e47d532420e54f5de8debc27b0f396
Remigiusz Ruszkiewicz <r.ruszkiewicz@microsoft.wsei.edu.pl>
2019-09-23 18:20:58 +0200

    Add project files.

---
+++ Logger/AppLogger.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Logger
+{
+    public class AppLogger
+    {
+        private readonly string path;
+        public AppLogger(string path)
+        {
+            this.path = path;
+        }
+        public void Save()
+        {
+            Save(path);
+        }
+        public void Save(string path)
+        {
+            if (!File.Exists(path))
+            {
+                File.Create(path).Close();
+            }
+            else
+            {
+                throw new Exception("Podaj inna nazwe");
+            }
+        }
+        public void Write(string text)
+        {
+            //File.WriteAllBytes(path, new byte[] { 123, 221, 14, 35, 67 });
+            File.AppendText(path).Close();
+
+        }
+        public void Write(string[] text)
+        {
+            File.AppendAllLines(path,text);
+        }
+        public void SaveDirectory(string dir)
+        {
+            if (!Directory.Exists(dir))
+            {
+                Directory.CreateDirectory(dir);
+            }
+            for (int i = 1; i <= 5; i++)
+            {
+                string path = Path.Combine(dir,$"{i}.txt");
+                Save(path);
+            }
+        }
+        public void Delete(string path = null)
+        {
+            if (path != null)
+            {
+                File.Delete(path);
+                return;
+            }
+            File.Delete(this.path);
+        }
+    }
+}
---
+++ LoggerTester/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Logger;
+
+namespace LoggerTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var logger = new AppLogger(@"D:\test.txt");
+            //logger.SaveDirectory(@"D:\Krzys");
+            ////logger.Save(@"D:\test.txt");
+            //string[] text = {"Anna","Roman"};
+            //logger.Write(text);
+            logger.Delete(@"D\Krzys\1.txt");
+            Console.Read();
+        }
+    }
+}
